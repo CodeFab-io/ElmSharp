f9b8f43558e5bc54f8e926a219d9147e0c812254
Fábio Beirão <fdbeirao@gmail.com>
2023-08-27 13:19:37 +0200

    📚 Add documentation to EmptyCommand and GetNewGuidCommand
    Contributes to #2

--- src/Commands/EmptyCommand.cs
+++ src/Commands/EmptyCommand.cs
@@ -4,6 +4,11 @@ public static partial class ElmSharp<TModel, TMessage>
 {
     public abstract partial class Command 
     {
+        /// <summary>
+        /// A command to be returned from either Init or Update when there is nothing to be done.
+        /// Since there is no data on this command, there is a Singleton instance available in
+        /// <see cref="Command.None"/>.
+        /// </summary>
         sealed class EmptyCommand : Command { }
     }
 }
--- src/Commands/GetNewGuidCommand.cs
+++ src/Commands/GetNewGuidCommand.cs
@@ -4,10 +4,14 @@ public static partial class ElmSharp<TModel, TMessage>
 {
     public abstract partial class Command 
     {
+        /// <summary>
+        /// The command to be used when you need to obtain a NewGuid. Uses <see cref="Guid.NewGuid"/> internally.
+        /// </summary>
         public sealed class GetNewGuidCommand : RunnableCommand 
         { 
             public Func<Guid, TMessage> OnNewGuidGenerated { get; init; }
 
+            /// <param name="onNewGuidGenerated">The message constructor capable of carrying a <see cref="Guid"/></param>
             public GetNewGuidCommand(Func<Guid, TMessage> onNewGuidGenerated) =>
                 OnNewGuidGenerated = onNewGuidGenerated;
 
