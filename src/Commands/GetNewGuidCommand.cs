f6464cd1f2e06fba3cc886a0bcd350125e24e5b2
Fábio Beirão <fdbeirao@gmail.com>
2023-08-27 13:06:39 +0200

    🔧 Add new built-in GetNewGuidCommand

---
+++ src/Commands/GetNewGuidCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace ElmSharp;
+
+public static partial class ElmSharp<TModel, TMessage>
+{
+    public abstract partial class Command 
+    {
+        public sealed class GetNewGuidCommand : RunnableCommand 
+        { 
+            public Func<Guid, TMessage> OnNewGuidGenerated { get; init; }
+
+            public GetNewGuidCommand(Func<Guid, TMessage> onNewGuidGenerated) =>
+                OnNewGuidGenerated = onNewGuidGenerated;
+
+            public override Task<TMessage?> Run()
+            {
+                var newGuid = Guid.NewGuid();
+
+                var message = OnNewGuidGenerated(newGuid);
+
+                return Task.FromResult<TMessage?>(message);
+            }
+        }
+    }
+}
---
+++ tst/ElmSharp-Tests/CommandTests/GetNewGuidCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using ElmSharp;
+
+namespace ElmSharp_Tests.CommandTests;
+
+public sealed class GetNewGuidCommandTests 
+{ 
+    sealed record TestModel();
+
+    abstract record TestMessage
+    {
+        internal sealed record NewGuidGenerated(Guid NewGuid) : TestMessage {}
+    }
+
+    [Fact]
+    public async Task GetNewGuidCommand_Generates_NewGuid() 
+    {
+        var sut = new ElmSharp<TestModel, TestMessage>.Command.GetNewGuidCommand(
+            onNewGuidGenerated: guid => new TestMessage.NewGuidGenerated(guid));
+        
+        var result = await sut.Run();
+
+        var msg = Assert.IsType<TestMessage.NewGuidGenerated>(result);
+        Assert.NotEqual(Guid.Empty, msg.NewGuid);
+    }
+}
